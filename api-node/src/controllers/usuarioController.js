e921122546e36569d6c0d9101fd16dd81f2b1a5c
GuilhermeAlvesBarros <guilherme.abarros@sptech.school>
2022-09-26 13:47:43 -0300

    cadastro

--- api-node/src/controllers/usuarioController.js
+++ api-node/src/controllers/usuarioController.js
@@ -64,6 +64,7 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var cpf = req.body.cpfServer;
     var senha = req.body.senhaServer;
 
     // Faça as validações dos valores
@@ -71,12 +72,14 @@ function cadastrar(req, res) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
+    }else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está undefined!");
     }else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, cpf, senha, email)
             .then(
                 function (resultado) {
                     res.json(resultado);
