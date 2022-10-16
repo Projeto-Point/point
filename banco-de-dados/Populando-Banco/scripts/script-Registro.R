1622cfeac583c40cb31b493baa2056b4a1782621
GuilhermeAlvesBarros <guilherme.abarros@sptech.school>
2022-10-16 12:34:05 -0300

    populando tabela registro

--- banco-de-dados/Populando-Banco/scripts/script-Registro.R
+++ banco-de-dados/Populando-Banco/scripts/script-Registro.R
@@ -1,13 +1,12 @@
 set.seed(123)
 
 registro <- data.frame(fkMaquina = 1,
-                       fkComponte = 1,
+                       fkComponte = 2,
                        dataEhora = seq(as.Date("2022/1/1"), as.Date("2022/10/14"), by = "day"),
-                       valor = sample(x = (5.50:90.90), 287, TRUE),
+                       valor = sample(x = (34.40:89.30), 287, TRUE),
                        unidadeDeMedida = "%"
-                       
                        )
-
+write.csv(registro,"C:\\Users\\corin\\OneDrive\\Área de Trabalho\\point\\point\\banco-de-dados\\Populando-Banco\\scripts\\script-registro-RAM.csv", row.names = FALSE)
 
 
 x <- as.Date("2022/1/1")
