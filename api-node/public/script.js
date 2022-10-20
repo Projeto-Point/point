13f17cc8daa4adbc624c3652af88e15e84e33858
IvanCCO <ivan.miranda@sptech.school>
2022-10-20 17:44:43 -0300

    fix: Redirecionamento de página pela logo

--- api-node/public/script.js
+++ api-node/public/script.js
@@ -1,10 +1,15 @@
 
-    console.log("a");
 function menu() {
-  var element = document.getElementById("menu-mobile");
-  element.classList.add("toggle");
+  var element = document.getElementById("iconMenu");
+  console.log("Entrou no menu")
+  // element.classList.add("toggle");
+ 
 }
 function Fechamenu() {
   var element = document.getElementById("navbar");
   element.classList.remove("toggle");
+}
+
+function voltarIndex(){
+  window.location = "index.html"
 }
--- banco-de-dados/Populando-Banco/scripts/script-Registro.R
+++ banco-de-dados/Populando-Banco/scripts/script-Registro.R
@@ -3,7 +3,7 @@ set.seed(123)
 registro <- data.frame(fkMaquina = 1,
                        fkComponte = 3,
                        dataEhora = seq(as.Date("2022/1/1"), as.Date("2022/10/14"), by = "day"),
-                       valor = seq(0, 1, length.out = 287),
+                       valor = seq(78.9, 90.9, length.out = 287),
                        unidadeDeMedida = "%"
 )
 write.csv(registro,"C:\\Users\\ivanm\\OneDrive\\Documentos\\SPTECH\\Projeto-Point-Sprint\\point\\banco-de-dados\\Populando-Banco\\base-de-dados\\tabela-registro-disco.csv", row.names = FALSE)
