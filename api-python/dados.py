6592ea229ee97afa8d586e90d06553bcb33d9678
gabrielYuuzoShimizu <gabriel.shimizu@sptech.school>
2022-10-31 12:43:53 -0300

    conectar-azure

--- api-python/dados.py
+++ api-python/dados.py
@@ -17,7 +17,28 @@ from dashing import (
     HGauge, 
     Text
 )
+
 import requests
+
+ # Para usar na AWS
+        # 1. sudo apt-get install unixodbc-dev
+        # 2. sudo apt-get install python-pip
+        # 3. pip install pyodbc
+import pyodbc as pyo
+
+        # Download driver (Windows)
+#https://go.microsoft.com/fwlink/?linkid=2202930
+
+        #Download driver (Linux)
+        # sudo su
+        # curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+        # curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+        # exit
+        # sudo apt-get update
+        # sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+pyo.connect("Driver={ODBC Driver 18 for SQL Server};Server=bd-point.database.windows.net,1433;Database=script-sql-server.sql;Uid=admin-point;Pwd=1cco#grupo1;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+
 
 import json
 # Pega os dados das máquina 
@@ -263,3 +284,126 @@ while True:
         sleep(3)
     except KeyboardInterrupt:
         break
+    
+    # conectando no Azure
+import psycopg2
+
+# Update connection string information
+
+host = "<bd-point.database.windows.net>"
+dbname = "<script-sql-server.sql>"
+user = "<admin-point>"
+password = "<1cco#grupo1>"
+sslmode = "require"
+
+# Construct connection string
+
+conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+conn = psycopg2.connect(conn_string)
+print("Connection established")
+
+cursor = conn.cursor()
+
+# Drop previous table of same name if one exists
+
+cursor.execute("DROP TABLE IF EXISTS Empresa;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Funcionario;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Telefone;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Endereco;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Maquina;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Componente;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Atributo;")
+print("Finished dropping table (if existed)")
+
+cursor.execute("DROP TABLE IF EXISTS Registro;")
+print("Finished dropping table (if existed)")
+
+# Create a table
+
+cursor.execute("CREATE TABLE Empresa (id empresa int PRIMARY KEY identity(1,1), name VARCHAR(45)not null, cnpj CHAR(11)unique not null, plano INT, descEmpresa varchar(45), constraint tipo_plano check (plano >= 1 and plano <= 3);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Funcionario (idFuncionario int primary key identity(1,1), nome varchar(45) not null, cpf char(11) unique not null, senha varchar(45) not null, email varchar(45) unique, fkGestor int,fkEmpresa int,foreign key (fkGestor) references funcionario (idFuncionario),foreign key (fkEmpresa) references empresa (idEmpresa);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Telefone (idTelefone int primary key identity(1,1),telefone char(11),fkFuncionario int,foreign key (fkFuncionario) references funcionario (idFuncionario);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Endereco (idEndereco int primary key identity(1,1),rua varchar(45),num int,bairro varchar(45),cidade VARCHAR(45) not null,estado char(2) not null,pais varchar(45) not null,fkFuncionario int,foreign key (fkFuncionario) references funcionario (idFuncionario),fkEmpresa int,foreign key (fkEmpresa) references empresa (idempresa);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Maquina (idMaquina int primary key identity(1,1),sistemaOperacional varchar(45) not null,nomeMaquina varchar(45),fkfuncionario int, foreign key (fkFuncionario) references funcionario (idFuncionario);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Componente (idComponente int primary key identity(1,1),tipo varchar(50),fkMaquina int,foreign key (fkMaquina) references maquina (idMaquina);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Atributo (idAtributo int primary key identity(1,1),atributo varchar(50),valor decimal(6,2),unidadeMedida varchar(30),fkComponente int,foreign key (fkComponente) references componente (idComponente);")
+print("Finished creating table")
+
+cursor.execute("CREATE TABLE Registro (idRegistro int primary key IDENTITY(1,1),valor decimal(6,2),unidadeMedida varchar(5),dataEhora datetime,fkComponente int,foreign key (fkComponente) references componente (idComponente);")
+print("Finished creating table")
+
+
+
+# Insert some data into the table
+
+cursor.execute("INSERT INTO Empresa (name, quantity) VALUES (%s, %s);", ("banana", 150))
+cursor.execute("INSERT INTO Funcionario (name, quantity) VALUES (%s, %s);", ("orange", 154))
+cursor.execute("INSERT INTO Telefone (name, quantity) VALUES (%s, %s);", ("apple", 100))
+cursor.execute("INSERT INTO Endereco (name, quantity) VALUES (%s, %s);", ("apple", 100))
+cursor.execute("INSERT INTO Maquina (name, quantity) VALUES (%s, %s);", ("apple", 100))
+cursor.execute("INSERT INTO Componente (name, quantity) VALUES (%s, %s);", ("apple", 100))
+cursor.execute("INSERT INTO Atributo (name, quantity) VALUES (%s, %s);", ("apple", 100))
+cursor.execute("INSERT INTO Registro (name, quantity) VALUES (%s, %s);", ("apple", 100))
+
+
+print("Inserted 8 rows of data")
+
+# Clean up
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
+# Fetch all rows from table
+
+cursor.execute("SELECT * FROM Empresa;")
+cursor.execute("SELECT * FROM Funcionario;")
+cursor.execute("SELECT * FROM Telefone;")
+cursor.execute("SELECT * FROM Endereco;")
+cursor.execute("SELECT * FROM Maquina;")
+cursor.execute("SELECT * FROM Componente;")
+cursor.execute("SELECT * FROM Atributo;")
+cursor.execute("SELECT * FROM Registro;")
+
+
+rows = cursor.fetchall()
+
+# Print all rows
+
+for row in rows:
+    print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+
+  # Update a data row in the table
+
+cursor.execute("UPDATE inventory SET quantity = %s WHERE name = %s;", (200, "banana"))
+print("Updated 1 row of data") 
+
+# Delete data row from table
+
+cursor.execute("DELETE FROM inventory WHERE name = %s;", ("orange",))
+print("Deleted 1 row of data")
