008ceb5c0469dca19d8abcb26caf561e582c2928
feehgrossi <felipe.leite@sptech.school>
2022-12-03 16:25:05 -0300

    felipe grossi esta enlouquecendo

--- api-node/src/controllers/grossizinhoController.js
+++ api-node/src/controllers/grossizinhoController.js
@@ -31,10 +31,29 @@ function horasAtivas(req, res){
     });
 }
 
+function entradaEsaida(req, res){
+    const idMaquina = req.query.idMaquina;
+    
+    grossizinhoModel.entradaEsaida(idMaquina)
+    .then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        }
+        else {
+            res.status(204).send("Nenhuma maquina encontrada!");
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao pegar a CPU: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 
 
 module.exports = {
     horasAtivas,
-    pegarRegistroInstancia
+    pegarRegistroInstancia,
+    entradaEsaida
     
 }
